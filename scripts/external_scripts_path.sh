88aa9720ee133f6a4016195974ec233f1bc9d8d8
Roberval Walsh <roberval.walsh@gmail.com>
2023-09-14 17:00:58 +0200

    fix

--- scripts/external_scripts_path.sh
+++ scripts/external_scripts_path.sh
@@ -2,4 +2,5 @@
 
 # To be effective, run the following command
 # . external_scripts.sh
-export PATH=$CMSSW_BASE/bin/slc7_amd64_gcc700/external:$PATH
+# export PATH=$CMSSW_BASE/bin/$SCRAM_ARCH/external:$PATH
+export PATH=$CMSSW_BASE/src/Analysis/Tools/scripts/external:$PATH
